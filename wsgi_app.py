e88b9d1e65c5c7077529cb24a666c2284096d320
Domingo <domingohui@gmail.com>
2016-10-05 23:48:00 -0400

    init

--- wsgi_app.py
+++ wsgi_app.py
@@ -1,7 +1,5 @@
 def app(environ, start_response):
 	"""A barebones WSGI application.
-
-	This is a starting point for your own Web framework :)
 	"""
 	status = '200 OK'
 	response_headers = [('Content-Type', 'text/plain')]
